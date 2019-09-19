a166f64b20ffac6c0f78189809a7237e7720cf0a
Gen <sophgen@gmail.com>
2019-09-18 21:01:24 -0500

    Intermediate changes

--- management_system/order_entry/admin.py
+++ management_system/order_entry/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from order_entry.models import order
+from order_entry.models import item
+from order_entry.models import item_price
+from order_entry.models import payment_type
 
 # Register your models here.
+admin.site.register(order)
+admin.site.register(item)
+admin.site.register(item_price)
+admin.site.register(payment_type)
--- management_system/order_entry/models.py
+++ management_system/order_entry/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
+class item(models.Model):
+    code = models.CharField(max_length=5)
+    description = models.CharField(max_length=200)
+    def __str__(self):
+        return f"({self.code}) - ({self.description})"
+
+class item_price(models.Model):
+    price_item = models.ForeignKey(item, on_delete=models.CASCADE)
+    price = models.FloatField()
+    price_effective_from = models.DateField()
+    price_effective_to = models.DateField()
+    def __str__(self):
+        return f"[{self.price_item}] - [{self.price}] - [{self.price_effective_from}] - [{self.price_effective_to}]"
+
+
+class payment_type(models.Model):
+    code = models.CharField(max_length=5)
+    type = models.CharField(max_length=200)
+    def __str__(self):
+        return f"[{self.code}] - [{self.type}]"
+
 # Create your models here.
+class order(models.Model):
+    order_desc = models.CharField(max_length=255)
+    order_date = models.DateField(auto_now=False)
+    order_item = models.ForeignKey(item, on_delete=models.CASCADE, related_name="item")
+    order_payment_type = models.ForeignKey(payment_type, on_delete=models.CASCADE, related_name="payment")
+    order_qty = models.IntegerField()
+    order_amount = models.FloatField()
+    order_created_date = models.DateTimeField(auto_now=True)
+    order_notes = models.CharField(max_length=1000)
+    def __str__(self):
+        return f"[{self.order_date}] - [{self.order_item}] - [{self.order_payment_type}] - [{self.order_amount}] - [{self.order_notes}]"
+
